10f3718f5d3e23105115860a3eb7590a71a930ab
Mokshith Voodarla <mvoodarla@gmail.com>
2023-04-22 18:03:12 -0700

    fix: whisper typing

--- workflows/speech_transcription/whisper.py
+++ workflows/speech_transcription/whisper.py
@@ -1,12 +1,13 @@
 import sieve
+from typing import Dict
 
 @sieve.workflow(name="audio_transcription_whisper")
-def audio_transcription(audio: sieve.Audio) -> sieve.Dict:
+def audio_transcription(audio: sieve.Audio) -> Dict:
     out = sieve.reference("sieve/whisper")(audio)
     return out
 
 @sieve.workflow(name="video_transcription_whisper")
-def video_transcription(video: sieve.Video) -> sieve.Dict:
+def video_transcription(video: sieve.Video) -> Dict:
     audio = sieve.reference("sieve/extract_audio")(video)
     out = sieve.reference("sieve/whisper")(audio)
     return out
